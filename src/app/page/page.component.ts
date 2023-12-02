3247309929b6af011cfa8e7c695058070ce33a89
Inbal Jona <inbaljona@hotmail.com>
2023-12-02 16:50:08 +0200

    add weather widget to page component

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { log } from 'console';
 
 @Component({
   selector: 'page',
@@ -8,6 +9,37 @@ import { CommonModule } from '@angular/common';
   templateUrl: './page.component.html',
   styleUrl: './page.component.scss'
 })
-export class PageComponent {
+export class PageComponent implements OnInit{
+
+  weatherData:any;
+  constructor(){}
+
+  ngOnInit(): void {
+    this.weatherData = {
+      main : {},
+      isDay: true
+    };
+    this.getWeatherData();
+    console.log(this.getWeatherData());
+  }
+
+  getWeatherData(){
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=tel+aviv&appid=7a8ceb0e6c5be276bbf7123eff1b0355")
+      .then(response=>response.json())
+      .then(data=>{this.setWeatherData(data);})
+    //let data = JSON.parse('[{"coord": {"lon": 48.570728,"lat": 34.790878},"country": "IR","geoname": {"cl": "P","code": "PPL", "parent": 132142} ]');
+    //this.setWeatherData(data);
+  }
+
+  setWeatherData(data : any){
+    this.weatherData = data;
+    let sunsetTime = new Date(this.weatherData.sys.sunset * 1000);
+    this.weatherData.sunset_time = sunsetTime.toLocaleTimeString();
+    let currentDate = new Date();
+    this.weatherData.isDay = (currentDate.getTime() < sunsetTime.getTime());
+    this.weatherData.temp_celcius = (this.weatherData.main.temp - 273.15).toFixed(0);
+    this.weatherData.temp_min = (this.weatherData.main.temp_min - 273.15).toFixed(0);
+    this.weatherData.temp_max = (this.weatherData.main.temp_max - 273.15).toFixed(0);
+  }
 
 }
