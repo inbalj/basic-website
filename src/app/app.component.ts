a0cadf2aaaa509cc7e420df826924b1d4d820087
Inbal Jona <inbaljona@hotmail.com>
2023-12-02 13:46:03 +0200

    fix form validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 import { HomeComponent } from './home/home.component';
 import { PageComponent } from './page/page.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
@@ -8,7 +10,7 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent, ContactFormComponent, PageComponent],
+  imports: [CommonModule, RouterOutlet,FormsModule, HomeComponent, ContactFormComponent, PageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,29 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { FormsModule, FormGroup, ReactiveFormsModule, FormBuilder, Validators} from '@angular/forms';
+
+
 
 @Component({
   selector: 'contact-form',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
 export class ContactFormComponent implements OnInit {
-  contactForm! : FormGroup;
+
+  contactForm!: FormGroup;
   payLoad = '';
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.contactForm = this.fb.group({
-      contactFormName: ['', Validators.required],
-      contactFormEmail: ['', [Validators.required, Validators.email]],
-      contactFormMessage: ['']
+
+     this.contactForm = this.fb.group({
+       name: ['', [ Validators.required,
+                    Validators.maxLength(50),
+                    Validators.pattern('^[a-zA-Z ]*$')]],
+       email: ['', [Validators.required, Validators.email]],
+       message: ['']
+     });
+
+     this.contactForm.statusChanges.subscribe(status => {
+      console.log('Form status:', status);
     });
   }
 
+
+
   onSubmit() {
-    this.payLoad = JSON.stringify(this.contactForm.value);
+    if (this.contactForm.valid) {
+      this.payLoad = JSON.stringify(this.contactForm.value);
+    }
+    else{
+     console.log('Form is invalid. Please check the fields.');
+    }
+
   }
 }
