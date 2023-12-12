b84727da50f19ee0ab6f4bb38bf069d916205336
Inbal Jona <inbaljona@hotmail.com>
2023-12-12 20:27:09 +0200

    add image caruosel+ angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 import { HomeComponent } from './home/home.component';
 import { PageComponent } from './page/page.component';
@@ -12,7 +14,7 @@ import { MessageItem } from '../shared/models/messageItem';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,FormsModule, HomeComponent, ContactFormComponent, PageComponent],
+  imports: [CommonModule, RouterOutlet,FormsModule, HomeComponent, ContactFormComponent, PageComponent, MatButtonModule, MatMenuModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CarouselImage, ImageCarouselComponent } from '../image-carousel/image-carousel.component';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ImageCarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  images : CarouselImage[] = [
+    {
+      url: 'https://thumbnails.production.thenounproject.com/WJPigpU9bCPb4XLjtIpyJttbDrg=/fit-in/1000x1000/photos.production.thenounproject.com/photos/food_and_drinks_for_two_on_a_tray-scopio-ab957dbf-9d59-4137-8d47-7df1de081b01.jpg',
+      caption: 'Food and drinks for two on a tray',
+      alt: ''
+    },
+    {
+      url: 'https://thumbnails.production.thenounproject.com/HgN-J2oTJH7jN0zk39PrjfZRZQ8=/fit-in/1000x1000/photos.production.thenounproject.com/photos/1FF713FB-6FE3-40C2-8AF1-D175E8B0BDBB.jpg',
+      caption: 'shishlik sticks',
+      alt: ''
+    },
+    {
+      url: 'https://thumbnails.production.thenounproject.com/SPLbXSt8SWWBo5BKyyb5EiGJIYI=/fit-in/1000x1000/photos.production.thenounproject.com/photos/D2F2DE27-EF48-488C-9790-4C0EB1918A3C.jpg',
+      caption: 'Dinner meal with five USDA food',
+      alt: '',
+    },
+    {
+      url: 'https://thumbnails.production.thenounproject.com/6FcAK8OtyFsQVRWfb6R6SGVfAR4=/fit-in/1000x1000/photos.production.thenounproject.com/photos/7D774C16-6CA9-4898-AEB5-A52FC76BBC8E.jpg',
+      caption: 'Dog sitting tall by food bowl',
+      alt: '',
+    },
+    {
+      url: 'https://thumbnails.production.thenounproject.com/70ejvUcUTigld4lsxz1i42v7BJo=/fit-in/1000x1000/photos.production.thenounproject.com/photos/297DEA21-7398-45C4-B24A-CF130FD47866.jpg',
+      caption: 'Healthy food and good morning text',
+      alt: '',
+    }
+
+  ];
 
 }
