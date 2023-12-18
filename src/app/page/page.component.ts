0fbae192fd8bb8ea5b5f6376ba30af69034161d3
Inbal Jona <inbaljona@hotmail.com>
2023-12-18 21:52:54 +0200

    change weather api provider

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -19,25 +19,31 @@ export class PageComponent implements OnInit{
       main : {},
       isDay: true
     };
-    this.getWeatherData();
-    console.log(this.getWeatherData());
+    this.getTodayWeatherData();
+    console.log(this.getTodayWeatherData());
   }
 
-  getWeatherData(){
+  /* getTodayWeatherData(){
     fetch("https://api.openweathermap.org/data/2.5/weather?q=tel+aviv&appid=7a8ceb0e6c5be276bbf7123eff1b0355")
       .then(response=>response.json())
-      .then(data=>{this.setWeatherData(data);})
+      .then(data=>{this.setTodayWeatherData(data);})
+  } */
+
+  getTodayWeatherData(){
+    fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/tel%20aviv/today?unitGroup=metric&include=current&key=6USUUHESZ2GF6GQZCQGDYXQ2X")
+      .then(response=>response.json())
+      .then(data=>{this.setTodayWeatherData(data);})
   }
 
-  setWeatherData(data : any){
+  setTodayWeatherData(data : any){
     this.weatherData = data;
-    let sunsetTime = new Date(this.weatherData.sys.sunset * 1000);
-    this.weatherData.sunset_time = sunsetTime.toLocaleTimeString();
-    let currentDate = new Date();
-    this.weatherData.isDay = (currentDate.getTime() < sunsetTime.getTime());
-    this.weatherData.temp_celcius = (this.weatherData.main.temp - 273.15).toFixed(0);
-    this.weatherData.temp_min = (this.weatherData.main.temp_min - 273.15).toFixed(0);
-    this.weatherData.temp_max = (this.weatherData.main.temp_max - 273.15).toFixed(0);
+
+    this.weatherData.isDay = this.weatherData.currentConditions.datetimeEpoch < this.weatherData.currentConditions.sunsetEpoch;
+    this.weatherData.tempCelcius = this.weatherData.currentConditions.temp;
+    this.weatherData.tempMin = this.weatherData.days[0].tempmin;
+    this.weatherData.tempMax = this.weatherData.days[0].tempmax;
+    this.weatherData.humidity = this.weatherData.currentConditions.humidity;
+    this.weatherData.feelLike = this.weatherData.currentConditions.feelslike;
   }
 
 }
