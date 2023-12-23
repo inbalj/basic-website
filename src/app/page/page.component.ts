bbc28c937dbc3f61d029f60d7497fa1d45f0a6f2
Inbal Jona <inbaljona@hotmail.com>
2023-12-23 17:23:41 +0200

    changing title to page component

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WeatherFormComponent } from '../weather-form/weather-form.component';
+import { Country } from '../../shared/models/country';
+import { City } from '../../shared/models/city';
 
 
 @Component({
@@ -12,6 +14,8 @@ import { WeatherFormComponent } from '../weather-form/weather-form.component';
 })
 export class PageComponent implements OnInit{
 
+  title: string = "Tel Aviv";
+
   weatherData : any;
   constructor(){}
 
@@ -21,17 +25,36 @@ export class PageComponent implements OnInit{
       isDay: true
     };
     this.getTodayWeatherData();
+    //this.getFormWeatherData();
+  }
+
+  setTitle(address : string){
+    this.title = address;
   }
 
   getTodayWeatherData(){
     fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/tel%20aviv/today?unitGroup=metric&include=current&key=6USUUHESZ2GF6GQZCQGDYXQ2X")
       .then(response=>response.json())
-      .then(data=>{this.setTodayWeatherData(data);})
+      .then(data=>{this.setWeatherData(data);})
   }
 
-  setTodayWeatherData(data : any){
-    this.weatherData = data;
+  getFormWeatherData(){
 
+    let country = "Israel";
+    let city = "Jerusalem";
+
+    fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+ country + "+" + city + "/today?unitGroup=metric&include=current&key=6USUUHESZ2GF6GQZCQGDYXQ2X")
+    .then(response=>response.json())
+    .then(data=>{this.setWeatherData(data);})
+  }
+
+
+
+  setWeatherData(data : any){
+    console.log( data.resolvedAddress);
+
+    this.weatherData = data;
+    this.setTitle(data.address);
     this.weatherData.isDay = this.weatherData.currentConditions.datetimeEpoch < this.weatherData.currentConditions.sunsetEpoch;
     this.weatherData.tempCelcius = this.weatherData.currentConditions.temp;
     this.weatherData.tempMin = this.weatherData.days[0].tempmin;
