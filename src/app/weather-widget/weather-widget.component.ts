00ea4a83bb732029e90ba588a9f40d69f49c6a69
Inbal Jona <inbaljona@hotmail.com>
2024-01-12 13:26:59 +0200

    updated the weather widget

--- src/app/weather-widget/weather-widget.component.ts
+++ src/app/weather-widget/weather-widget.component.ts
@@ -28,10 +28,12 @@ export class WeatherWidgetComponent implements OnInit{
   ngOnInit(): void {
     this.weatherData = {
       main : {},
-      isDay: true
+      isDay: true,
+      isCloudy: false,
+      isRainy: false
     };
     this.getTodayWeatherData();
-
+    // subscribe to the weatherService to update the data according to the service
     this.weatherService.weatherData$.subscribe(data => {
       this.choosenCity = data.choosenCity;
       this.choosenCountry = data.choosenCountry;
@@ -55,17 +57,15 @@ export class WeatherWidgetComponent implements OnInit{
   }
 
   setWeatherData(data : any){
-
     this.weatherData = data;
-    this.weatherData.isDay = this.weatherData.currentConditions.datetimeEpoch < this.weatherData.currentConditions.sunsetEpoch;
+    this.weatherData.isDay = ((this.weatherData.currentConditions.datetimeEpoch < this.weatherData.currentConditions.sunsetEpoch)&&(this.weatherData.currentConditions.datetimeEpoch > this.weatherData.currentConditions.sunriseEpoch));
     this.weatherData.tempCelcius = this.weatherData.currentConditions.temp;
     this.weatherData.tempMin = this.weatherData.days[0].tempmin;
     this.weatherData.tempMax = this.weatherData.days[0].tempmax;
     this.weatherData.humidity = this.weatherData.currentConditions.humidity;
     this.weatherData.feelLike = this.weatherData.currentConditions.feelslike;
-    this.weatherData.isCloudy = (this.weatherData.currentConditions.condition == 'cloudy')? true : false;
-    this.weatherData.isRainy = (this.weatherData.currentConditions.condition == 'Rain')? true : false;
+    this.weatherData.isCloudy = this.weatherData.currentConditions.conditions.includes('cloudy');
+    this.weatherData.isRainy = this.weatherData.currentConditions.conditions.includes('Rain');
   }
-
 
 }
